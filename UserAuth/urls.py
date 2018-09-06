21bb018cc02eb6c46e7b9c0a1ee6ddc56e3f1ed8
Sanyem <sanyem.g16@iiits.in>
2018-09-07 02:21:04 +0530

    Login SignUp Complete

--- UserAuth/urls.py
+++ UserAuth/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'UserAuth'
 
@@ -9,4 +11,8 @@ urlpatterns = [
 		url(r'^signup/$', views.signupUser, name='signup'),
 		url(r'^signup-professor/$', views.signupProfessor, name='signup-professor'),
 		url(r'^signup-student/$', views.signupStudent, name='signup-student'),
-	]
+		url(r'^login-professor/$', views.loginProfessor, name='login-professor'),
+		url(r'^login-student/$', views.loginStudent, name='login-student'),
+
+		url(r'^show-error/$', views.showError, name='show-error'),
+	]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- UserAuth/views.py
+++ UserAuth/views.py
@@ -9,19 +9,20 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login
 from Student.models import *
 from Professor.models import *
+from django.contrib import messages
+import string, re
 
 def landingPage(request):
 	if request.method == 'GET':
-		print('wfwfw')
 		if request.user.is_authenticated:
-			print('here now now')
 			if request.user.groups.filter(name='Professor').exists():
-				print('professor')
 				return HttpResponseRedirect(reverse('Professor:dashboard'))
 			elif request.user.groups.filter(name='Student').exists():
-				print('stuednt')
 				return HttpResponseRedirect(reverse('Student:dashboard'))
 		return render(request, 'UserAuth/landingPage.html')
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
 
 def loginUser(request):
 	if request.method == 'GET':
@@ -31,16 +32,22 @@ def loginUser(request):
 			elif request.user.groups.filter(name='Student').exists():
 				return HttpResponseRedirect(reverse('Student:dashboard'))
 		return render(request, 'UserAuth/loginPage.html')
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
 
 def signupUser(request):
 	if request.method == 'GET':
-		if request.user.is_authernticated:
+		if request.user.is_authenticated:
 			if request.user.groups.filter(name='Professor').exists():
 				return HttpResponseRedirect(reverse('Professor:dashboard'))
 			elif request.user.groups.filter(name='Student').exists():
 				return HttpResponseRedirect(reverse('Student:dashboard'))
 		gender_choices = GenderChoice.objects.all()
 		return render(request, 'UserAuth/signupPage.html', {'gender_choices': gender_choices})
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
 
 def signupProfessor(request):
 	if request.method == 'POST':
@@ -56,7 +63,8 @@ def signupProfessor(request):
 		otp = randint(100000, 999999)
 
 		if User.objects.filter(is_superuser=False, email=email).count() > 0 or ProfessorAuthProfile.objects.filter(mobile_no=mobile_no).count() > 0 or StudentAuthProfile.objects.filter(mobile_no=mobile_no).count() > 0:
-			return HttpResponse('user already signed up with this mobile no or email')
+			messages.warning(request, 'user already signed up with this mobile no or email')
+			return HttpResponseRedirect(reverse('UserAuth:signup'))
 		else:
 			gender = GenderChoice.objects.get(name=gender)
 
@@ -92,7 +100,9 @@ def signupProfessor(request):
 			if user is not None:
 				login(request, user)
 				return HttpResponseRedirect(reverse('Professor:dashboard'))
-	return HttpResponse('professor signup')
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
 
 def signupStudent(request):
 	if request.method == 'POST':
@@ -108,7 +118,8 @@ def signupStudent(request):
 		otp = randint(100000, 999999)
 
 		if User.objects.filter(is_superuser=False, email=email).count() > 0 or ProfessorAuthProfile.objects.filter(mobile_no=mobile_no).count() > 0 or StudentAuthProfile.objects.filter(mobile_no=mobile_no).count() > 0:
-			return HttpResponse('user already signed up with this mobile no or email')
+			messages.warning(request, 'user already signed up with this mobile no or email')
+			return HttpResponseRedirect(reverse('UserAuth:signup'))
 		else:
 			gender = GenderChoice.objects.get(name=gender)
 
@@ -144,4 +155,93 @@ def signupStudent(request):
 			if user is not None:
 				login(request, user)
 				return HttpResponseRedirect(reverse('Student:dashboard'))
-	return HttpResponse('student signup')
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
+
+def showError(request):
+	return render(request, 'UserAuth/error.html')
+
+def loginProfessor(request):
+	if request.method == 'POST':
+		username = request.POST.get('professor_login_username')
+		password = request.POST.get('professor_login_password')
+
+		if username.isdigit():
+			try:
+				instance = ProfessorAuthProfile.objects.get(mobile_no=username)
+				username = instance.user.username
+				user = authenticate(username=username, password=password)
+				if user is not None:
+					login(request, user)
+					return HttpResponseRedirect(reverse('Professor:dashboard'))
+				messages.warning(request, "Phone No and password doesn't match with any GURUKOOL PROFESSOR account.")
+				return HttpResponseRedirect(reverse('UserAuth:login'))
+			except:
+				messages.warning(request, "No GURUKOOL PROFESSOR account is registered with that mobile no.")
+				return HttpResponseRedirect(reverse('UserAuth:login'))
+		else:
+			try:
+				match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', username)
+				if match is None:
+					messages.warning(request, 'E-mail entered is not a valid email-address')
+					return HttpResponseRedirect(reverse('UserAuth:login'))
+				try:
+					instance = ProfessorAuthProfile.objects.get(email_address=username)
+				except:
+					messages.warning(request, 'No GURUKOOL PROFESSOR account registered with this email-address.')
+					return HttpResponseRedirect(reverse('UserAuth:login'))
+				user = authenticate(username=username, password=password)
+				if user is not None:
+					login(request, user)
+					return HttpResponseRedirect(reverse('Student:dashboard'))
+				messages.warning(request, "Email address and Password does not match to any GURUKOOL PROFESSOR Account.")
+				return HttpResponseRedirect(reverse('UserAuth:login'))
+			except:
+				messages.error(request, 'BAD REQUEST')
+				return HttpResponseRedirect(reverse('UserAuth:show-error'))
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
+
+def loginStudent(request):
+	if request.method == 'POST':
+		username = request.POST.get('student_login_username')
+		password = request.POST.get('student_login_password')
+
+		if username.isdigit():
+			try:
+				instance = StudentAuthProfile.objects.get(mobile_no=username)
+				username = instance.user.username
+				user = authenticate(username=username, password=password)
+				if user is not None:
+					login(request, user)
+					return HttpResponseRedirect(reverse('Student:dashboard'))
+				messages.warning(request, "Phone No and password doesn't match with any GURUKOOL STUDENT account.")
+				return HttpResponseRedirect(reverse('UserAuth:login'))
+			except:
+				messages.warning(request, "No GURUKOOL STUDENT account is registered with that mobile no.")
+				return HttpResponseRedirect(reverse('UserAuth:login'))
+		else:
+			try:
+				match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', username)
+				if match is None:
+					messages.warning(request, 'E-mail entered is not a valid email-address')
+					return HttpResponseRedirect(reverse('UserAuth:login'))
+				try:
+					instance = StudentAuthProfile.objects.get(email_address=username)
+				except:
+					messages.warning(request, 'No GURUKOOL STUDENT account registered with this email-address.')
+					return HttpResponseRedirect(reverse('UserAuth:login'))
+				user = authenticate(username=username, password=password)
+				if user is not None:
+					login(request, user)
+					return HttpResponseRedirect(reverse('Student:dashboard'))
+				messages.warning(request, "Email address and Password does not match to any GURUKOOL STUDENT Account")
+				return HttpResponseRedirect(reverse('UserAuth:login'))
+			except:
+				messages.error(request, 'BAD REQUEST')
+				return HttpResponseRedirect(reverse('UserAuth:show-error'))
+	else:
+		messages.error(request, 'BAD REQUEST')
+		return HttpResponseRedirect(reverse('UserAuth:show-error'))
