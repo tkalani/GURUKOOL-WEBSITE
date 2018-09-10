1e1aa362fcbfbd3a526e574e059c9bf91a81d809
bhavi289 <bhavi.c16@iiits.in>
2018-09-10 14:29:27 +0530

    changing database settings, shifting to mysql

--- gurukool_website/settings.py
+++ gurukool_website/settings.py
@@ -84,8 +84,10 @@ WSGI_APPLICATION = 'gurukool_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gurukool_db',
+        'USER': 'root',
+        'PASSWORD': 'bhavi',
     }
 }
 
