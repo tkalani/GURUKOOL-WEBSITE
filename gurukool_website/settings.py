405efb2750ae8fc4dc2dcc1aabd4b5514fb2b03d
Sanyem <sanyem.g16@iiits.in>
2018-10-24 19:48:39 +0530

    init file change

--- gurukool_website/settings.py
+++ gurukool_website/settings.py
@@ -105,6 +105,13 @@ WSGI_APPLICATION = 'gurukool_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -115,7 +122,6 @@ DATABASES = {
         'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sanyem/Desktop/ASE_project/GURUKOOL-WEBSITE/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
