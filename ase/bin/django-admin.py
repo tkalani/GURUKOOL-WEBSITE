8ba56a6970e0945a948b7db778de7f814d33518c
Sanyem <sanyem.g16@iiits.in>
2018-10-24 23:36:09 +0530

    without env

--- ase/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/sanyem/Desktop/ASE_project/GURUKOOL-WEBSITE/ase/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/sanyem/Desktop/ASE_project/GURUKOOL-WEBSITE/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
