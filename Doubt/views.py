76fec4f8d8cf98458e1979838ed98e942c040543
Sanyem <sanyem.g16@iiits.in>
2018-10-26 13:33:50 +0530

    comments Doubt,User Auth

--- Doubt/views.py
+++ Doubt/views.py
@@ -10,6 +10,9 @@ prof_group = 'Professor'
 login_url = 'UserAuth:login'
 
 def group_required(*group_names, login_url=None):
+    ''' checks group requirement whwther the user is student or professor
+        Takes group name as input and login url
+        returns status if user passes or not'''
     def in_groups(u):
         if u.is_authenticated:
             if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
@@ -17,18 +20,27 @@ def group_required(*group_names, login_url=None):
         return False
     return user_passes_test(in_groups, login_url=login_url)
 
-
+''' login required'''
 @login_required(login_url=login_url)
 @group_required(prof_group, login_url=login_url)
 def show_doubt(request, course_id):
+    '''
+        Shows all Doubts
+        Takes input course_id,request
+        Returns doubt_list,course and renders the web page
+    '''
     if request.method == 'GET':
         doubts = Doubt.objects.filter(course__id=course_id)
         course = Course.objects.get(id=course_id)
-        # print(doubts)
         return render(request, 'Doubt/doubt.html', {"doubt_list": doubts, "course":course})
 
 @login_required(login_url=login_url)
 def create_comment(request, doubt_id):
+    '''
+       Creates a new comment
+       Takes input request method and doubt_id
+       returns http response if comment created successfuly 
+    '''
     if request.method == 'POST':
         text = request.POST['comment']
         doubt = Doubt.objects.get(id=doubt_id)
@@ -39,6 +51,11 @@ def create_comment(request, doubt_id):
 @login_required(login_url=login_url)
 @group_required(prof_group, login_url=login_url)
 def doubt(request, doubt_id):
+    '''
+        Get a particular doubt
+        Takes input request method and doubt_id
+        Returns doubt_data,previous_replies and renders the web page
+    '''
         if request.method == 'GET':
                 try:
                         doubt = Doubt.objects.get(id=doubt_id)
