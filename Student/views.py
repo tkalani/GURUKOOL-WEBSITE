76585a163129d4f0ae2600ad85e72e2bb6b8be5f
tkalani <tanmay.k16@iiits.in>
2018-12-09 13:39:23 +0530

    Filters, Profile

--- Student/views.py
+++ Student/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.urlresolvers import reverse
 from .models import *
+from Doubt.models import *
+from Meeting.models import *
+from Professor.models import *
 
 group_name = 'Student'
 login_url = 'UserAuth:landingPage'
@@ -26,7 +29,17 @@ def profile(request, student_id):
         results = QuizResult.objects.filter(student__user__user__id=student_id).order_by('-conduct_quiz__conduction_date')
         student_profile = StudentProfile.objects.get(user__user__id=student_id)
         courses = CourseStudent.objects.filter(student__user__user__id=student_id)
-        return render(request, 'Student/profile.html', {"results": results, "student_profile": student_profile, "courses": courses})
+
+        meetings = MeetingPlace.objects.filter(meeting__student__user__user__id=student_id)
+
+        course_list = CourseProfessor.objects.filter(professor__user__user__id=request.user.id).order_by('-id')
+        all_doubt_list = []
+        for course in course_list:
+                for y in Doubt.objects.filter(course__id=course.course.id, student__user__user__id=student_id):
+                        all_doubt_list.append(y)
+        all_doubt_list = sorted(all_doubt_list, key=lambda k: k.last_updated)
+        all_doubt_list = all_doubt_list[::-1]
+        return render(request, 'Student/profile.html', {"meetings": meetings, "doubts": all_doubt_list, "results": results, "student_profile": student_profile, "courses": courses})
     except Exception as e:
         print(e)
         return HttpResponse("This student doesn't exist")
